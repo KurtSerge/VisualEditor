2c2f8e8d93c414ae26d490c41d5af1fe2052de08
unknown <KurtSerge@gmail.com>
2013-07-01 23:35:27 +0200

    missing file

---
+++ Editor/src/EditorFramework/ConstructFactory.java
@@ -0,0 +1,20 @@
+package EditorFramework;
+
+import java.util.Map;
+
+
+public class ConstructFactory {
+	// Map of URI's to Construct
+	private Map<String, Class> m_RegisteredConstructs;
+
+	public void registerConstruct (String URI, Class constructClass) {
+	}
+
+	public Construct createConstruct(String URI)
+	{
+		return null;
+	}
+}
+
+
+	
--- Editor/src/EditorFramework/SyntaxTreeElement.java
+++ Editor/src/EditorFramework/SyntaxTreeElement.java
@@ -5,4 +5,5 @@ import java.util.List;
 public class SyntaxTreeElement  {
 	public String literal;
 	public String URI; // class name, in this case
+	public Construct construct;
 }
