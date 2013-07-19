e3ea28aa3d794ba904895a8cf04667e48a236b5b
unknown <KurtSerge@gmail.com>
2013-07-19 22:38:21 +0200

    Fixes to merge

--- EditorDemo/src/editor/Application.java
+++ EditorDemo/src/editor/Application.java
@@ -123,67 +123,6 @@ public class Application extends JFrame
 			//}
 		}
 	}
-	
-    private class MyDispatcher implements KeyListener {
-    	boolean insert_pressed;
-    	private final JFrame frame;
-    	
-    	public MyDispatcher(JFrame frame) {
-    		this.frame = frame;
-    	}
-    	
-		@Override
-		public void keyPressed(KeyEvent e) {
-			
-            if (e.getID() == KeyEvent.KEY_PRESSED) {
-            	
-        		// Check for combo key presses, such as "i + o"
-    			if(insert_pressed==true) {
-            		insert_pressed = false;
-	        		switch(e.getKeyCode()) {
-	        			// Insert KV pair
-	        		/*
-		        		case KeyEvent.VK_K: {
-		        			JSONObject obj=new JSONObject();
-		        			obj.put("temp","temp");// TODO: second string should actually be ? placeholder... can be object OR string
-		        			Construct ret = JSONController.add_key_value_pair(null, controller.getSelectedEditor().construct.parent);
-		        			if(ret != null)
-		        				JSONController.editors_from_constructs(ret);
-		        			break;
-		        		}
-		        		case KeyEvent.VK_O: {
-		        			JSONObject obj=new JSONObject();
-		        			obj.put("temp",new JSONObject());// TODO: second string should actually be ? placeholder... can be object OR string
-		        			Construct ret = JSONController.add_key_value_pair(obj, controller.getSelectedEditor().construct);
-		        			if(ret != null)
-		        				JSONController.editors_from_constructs(ret);
-		        		}*/
-	        		}
-	        		return;
-    			}
-    			
-        		// Reset first key press
-        		insert_pressed = false;
-        		
-        		switch(e.getKeyCode()) {
-	        		case KeyEvent.VK_I:
-	        			insert_pressed = true;
-	        			repaint();
-	        			break;
-	        		case KeyEvent.VK_Q:
-	        			jsonDocumentConstruct2.debugPrint();
-	        			break;
-	        		default:
-	        			break;
-        		}
-            }
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {}
-		@Override
-		public void keyTyped(KeyEvent e) {}
-    }
     
     // Using AWT because Swing "MouseListener" doesn't give coords when clicking over jtextareas
     private static class Listener implements AWTEventListener {
@@ -200,7 +139,7 @@ public class Application extends JFrame
 	{
 		super("Editor Demo");
 		
-		boolean shouldLoadJson = false;	// Alt: Loads Clojure
+		boolean shouldLoadJson = true;	// Alt: Loads Clojure
 		
 		this.setSize(800, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -259,29 +198,26 @@ public class Application extends JFrame
 		
 		this.add(top);
 		
-//<<<<<<< HEAD
-		controller = new BaseController(this, ClojureController.editors);
-		top.addKeyListener(controller);
 
-		Toolkit.getDefaultToolkit().addAWTEventListener(new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
-//=======
-//		controller = new BaseController(this, JSONController.editors);
-//		top.addKeyListener(controller); // Must add BaseController first
-		//top.addKeyListener(new MyDispatcher(this));
-		
-//		controller.setListener(new HotkeyListener());
-//		controller.registerHotkey(EKeyBinding.Bind_InsertAfter, "IA?");
-//		controller.registerHotkey(EKeyBinding.Bind_InsertBefore, "IB?");
-//		controller.registerHotkey(EKeyBinding.Bind_InsertReplace, "IR?");
-//		
-//
-//
-//	
-//		//top.addMouseListener(new MouseSelector());
-//		  Toolkit.getDefaultToolkit().addAWTEventListener(
-//		          new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
-//		  
-//>>>>>>> abfa3150d52f75d29b6d4d0604c110ac765611e5
+		if(shouldLoadJson == false) {
+			controller = new BaseController(this, ClojureController.editors);
+			top.addKeyListener(controller);
+	
+			Toolkit.getDefaultToolkit().addAWTEventListener(new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
+		}
+		else{
+			controller = new BaseController(this, JSONController.editors);
+			top.addKeyListener(controller); // Must add BaseController first
+
+			controller.setListener(new HotkeyListener());
+			controller.registerHotkey(EKeyBinding.Bind_InsertAfter, "IA?");
+			controller.registerHotkey(EKeyBinding.Bind_InsertBefore, "IB?");
+			controller.registerHotkey(EKeyBinding.Bind_InsertReplace, "IR?");
+			
+			//top.addMouseListener(new MouseSelector());
+			Toolkit.getDefaultToolkit().addAWTEventListener(new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
+
+		}
 
 		this.pack();
 		this.setSize(800, 600);
