30fd4044948212af403e4dc7c7b1e0c2510a1a47
Christopher Lord <chris.l.lord@gmail.com>
2013-07-26 13:59:51 +0100

    Migrate JSONHotkeyListener

--- EditorDemo/src/editor/Application.java
+++ EditorDemo/src/editor/Application.java
@@ -23,10 +23,10 @@ import editor.document.JSONConstructDocument;
 import clojure.ClojureController;
 
 import json.JSONController;
-
+import json.JSONHotkeyListener;
 public class Application extends JFrame
 {
-	private void setupNewConstruct(Component top) {
+	private void setupNewConstruct(Component top, BaseControllerListener listener) {
 		// Delete
 		if(controller != null) {
 			controller.getSelectedEditor().deleteAll();
@@ -38,7 +38,7 @@ public class Application extends JFrame
 		top.addKeyListener(controller);
 		top.requestFocus();
 		
-		controller.setListener(new HotkeyListener(mDocument, this));
+		controller.setListener(listener);
 		controller.registerHotkey(EKeyBinding.Bind_InsertAfter, String.format("%s%s%s", (char)KeyEvent.VK_I, (char)KeyEvent.VK_A, (char)KeyEvent.VK_UNDEFINED));
 		controller.registerHotkey(EKeyBinding.Bind_InsertBefore, String.format("%s%s%s", (char)KeyEvent.VK_I, (char)KeyEvent.VK_B, (char)KeyEvent.VK_UNDEFINED));
 		controller.registerHotkey(EKeyBinding.Bind_InsertReplace, String.format("%s%s%s", (char)KeyEvent.VK_I, (char)KeyEvent.VK_R, (char)KeyEvent.VK_UNDEFINED));
@@ -57,154 +57,7 @@ public class Application extends JFrame
 	private ConstructDocument mDocument = null;
 	private BaseController controller = null;
 	
-	private class HotkeyListener implements BaseControllerListener {
-		private final ConstructDocument mDocument;
-		private final JFrame window;		
 
-		public HotkeyListener(ConstructDocument document, JFrame window) {
-			this.window = window;
-			this.mDocument = document;
-		}
-		
-		@Override
-		public void receivedHotkey(BaseController controller, EKeyBinding binding, int keyEventCode) {
-			handleInsert(binding, keyEventCode);
-		}
-
-		private void handleInsert(EKeyBinding binding, int keyEventCode) {
-			Construct newConstruct = null;
-			Construct parent = null;
-
-			// Determine parent of construct to be added
-			switch(binding) {
-				case Bind_InsertAfter:  
-				case Bind_InsertBefore:
-				case Bind_InsertReplace:
-				case Bind_InsertUsurp:
-					parent = controller.getSelectedEditor().construct.parent;
-					break;
-				case Bind_InsertChild:
-					parent = controller.getSelectedEditor().construct;
-					break;
-					
-				case Bind_Undo:
-					mDocument.undo();
-					return;
-					
-				case Bind_Redo:
-					mDocument.redo();
-					return;
-					
-				case Bind_DebugPrint:
-					mDocument.debugPrint();
-					break;
-					
-				default:
-					throw new RuntimeException("Unhandled hotkey");
-			
-			}
-			
-			// Get a construct to insert
-			switch(keyEventCode) {
-				case KeyEvent.VK_O: // Object
-					if(parent.getClass() == json.KeyValueConstruct.class)  {
-						// Empty Object 
-						JSONObject newObj = new JSONObject();
-						newConstruct = JSONController.construct_for_json(newObj, parent);
-					}
-					else {
-						// KV-Pair with Obj value (it's common enough to warrant a hotkey methinks)
-						// FIXME: needs work
-						JSONObject newObj = new JSONObject();
-						newObj.put("EmptyString", new JSONObject());
-						newConstruct = JSONController.construct_for_json(newObj, parent);
-					}
-					break;
-				case KeyEvent.VK_S: // String
-					newConstruct = JSONController.construct_for_json("EmtpyStr", parent);
-					break;
-				case KeyEvent.VK_K: // KVPair
-        			newConstruct = JSONController.get_empty_kvp(parent);
-					break;
-				case KeyEvent.VK_I: // Integer
-					newConstruct = JSONController.construct_for_json(0, parent);
-					break;
-				case KeyEvent.VK_F: // Float
-					newConstruct = JSONController.construct_for_json(0.0, parent);
-					break;
-				case KeyEvent.VK_B: // Bool
-					newConstruct = JSONController.construct_for_json(true, parent);
-					break;
-				case KeyEvent.VK_N: // Null
-					newConstruct = JSONController.construct_for_json(org.json.JSONObject.NULL, parent);
-					break;
-				case KeyEvent.VK_A: // Array
-					JSONArray list = new JSONArray();
-					list.put("test");
-					list.put("test2");
-					newConstruct = JSONController.construct_for_json(list, parent);
-					break;
-				default:
-					return;
-			}
-			// Error check previous step
-			if(newConstruct == null)
-				return;
-			
-			// Determine location of insertion
-			int selIndex = parent.children.indexOf(controller.getSelectedEditor().construct);
-			switch(binding) {
-				case Bind_InsertAfter: {
-					parent.addChild(selIndex + 1, newConstruct);
-					break;
-				}
-				case Bind_InsertBefore:  {
-					parent.addChild(selIndex,  newConstruct);
-					break;
-				}
-				case Bind_InsertReplace:  {
-					ConstructEditor editorParent = controller.getSelectedEditor().getParent();
-					editorParent.replaceChild(controller.getSelectedEditor().construct, newConstruct);
-					break;
-				}
-				case Bind_InsertChild: {
-					int lastIndex = controller.getSelectedEditor().construct.children.size();
-					controller.getSelectedEditor().construct.addChild(lastIndex, newConstruct);
-					break;
-				}
-				case Bind_InsertUsurp: {
-					// FIXME: What to test this on?
-					if(1==1)
-						throw(new RuntimeException("Usurp not working"));
-					
-					parent.addChild(selIndex + 1, newConstruct);// TODO: delete this if usurp fails
-					// Copy children
-					for(Construct child : controller.getSelectedEditor().construct.children)  {
-						Construct usurp = child.deepCopy(newConstruct);
-						int addIndex = newConstruct.children.size();
-						parent.addChild(addIndex, usurp);
-					}
-					
-					// copy selected children
-					//newConstruct = controller.getSelectedEditor().construct.deepCopy(parent);
-					//parent.addChild(selIndex+1, newConstruct);
-					
-					// delete selected children
-					//parent.deleteChild(controller.getSelectedEditor().construct);
-					break;
-				}
-				
-				default:
-					break;
-			}
-			
-			ConstructEditor added = mDocument.editorsFromConstruct(newConstruct);
-			if(added != null)  {
-				controller.getSelectedEditor().update();
-				controller.selector.Select(added);
-			}
-		}
-	}
     
 	
 	Application()
@@ -218,27 +71,45 @@ public class Application extends JFrame
 		this.setVisible(true);
 		this.setBackground(Color.white);
 		
+		final JFrame window = this;
+		
 		try {
 			if(shouldLoadJson) {
 				mDocument = new JSONConstructDocument("test2.json");
+				mDocument.setListener(new ConstructDocument.ConstructDocumentListener() {
+					@Override
+					public void onDocumentUndo(ConstructDocument document) {
+						JSONHotkeyListener keyListener = new JSONHotkeyListener(controller, document, window);
+						setupNewConstruct(document.getRootComponent(), keyListener);
+					}
+					
+					@Override
+					public void onDocumentRedo(ConstructDocument document) {
+						JSONHotkeyListener keyListener = new JSONHotkeyListener(controller, document, window);
+						setupNewConstruct(document.getRootComponent(), keyListener);
+					}
+				});				
 			} else {
 				mDocument = new ClojureConstructDocument("testtypes.clj");
+				mDocument.setListener(new ConstructDocument.ConstructDocumentListener() {
+					@Override
+					public void onDocumentUndo(ConstructDocument document) {
+						clojure.HotkeyListener keyListener = new clojure.HotkeyListener(document);
+						setupNewConstruct(document.getRootComponent(), keyListener);
+					}
+					
+					@Override
+					public void onDocumentRedo(ConstructDocument document) {
+						clojure.HotkeyListener keyListener = new clojure.HotkeyListener(document);
+						setupNewConstruct(document.getRootComponent(), keyListener);
+					}
+				});				
 			}
 			
 			// Adds a listener that will update the 
 			// visible document when the document is
 			// updated via redo/undo methods.
-			mDocument.setListener(new ConstructDocument.ConstructDocumentListener() {
-				@Override
-				public void onDocumentUndo(ConstructDocument document) {
-					setupNewConstruct(document.getRootComponent());
-				}
-				
-				@Override
-				public void onDocumentRedo(ConstructDocument document) {
-					setupNewConstruct(document.getRootComponent());
-				}
-			});
+
 		
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
@@ -264,7 +135,8 @@ public class Application extends JFrame
 			controller.registerHotkey(EKeyBinding.Bind_InsertReplace, String.format("%s%s%s", (char)KeyEvent.VK_I, (char)KeyEvent.VK_R, (char)KeyEvent.VK_UNDEFINED));
 		}
 		else{
-			setupNewConstruct(rootDocumentComponent);
+			JSONHotkeyListener keyListener = new JSONHotkeyListener(controller, mDocument, this);
+			setupNewConstruct(rootDocumentComponent, keyListener);
 			Toolkit.getDefaultToolkit().addAWTEventListener(new BaseMouseController(controller, mDocument), AWTEvent.MOUSE_EVENT_MASK);
 		}
 
