8bb1eb1b4778cbe125012c86bc920f0ada6a1d15
Christopher Lord <chris.l.lord@gmail.com>
2013-08-01 20:21:31 +0100

    Adding ConstructPublisher

---
+++ EditorDemo/src/editor/ConstructPublisher.java
@@ -0,0 +1,62 @@
+package editor;
+
+import editor.document.ConstructDocument;
+
+public class ConstructPublisher {
+	
+	public static interface ConstructListener { 
+		public void onConstructModified(Construct construct);
+		public void onConstructAddedChild(Construct parent, Construct child, int index);
+		public void onConstructRemovedChild(Construct parent, Construct child, int index);
+	}
+
+	private static ConstructPublisher sInstance = null;
+	private ConstructDocument mDocument = null;
+	private ConstructListener mListener = null;
+	
+	public static ConstructPublisher getInstance() { 
+		if(sInstance == null) {
+			sInstance = new ConstructPublisher();
+		}
+		
+		return sInstance;
+	}
+	
+	public void addListener(ConstructListener listener) { 
+		mListener = listener;
+	}
+
+	public void setActiveDocument(ConstructDocument document) {
+		mDocument = document;
+	}
+	
+	protected boolean isConstructInDocument(Construct construct) { 
+		if(mDocument == null)
+			return false;
+		
+		if(construct.parent != null) {
+			return isConstructInDocument(construct.parent);
+		}
+		
+		return construct.equals(mDocument.getRootConstruct());
+	}
+	
+	public void onConstructAddedChild(Construct parent, Construct child, int index) { 
+		if(this.isConstructInDocument(parent)) { 
+			mListener.onConstructAddedChild(parent, child, index);
+		}
+	}
+	
+	public void onConstructRemovedChild(Construct parent, Construct child, int index) { 
+		if(this.isConstructInDocument(parent)) { 
+			mListener.onConstructRemovedChild(parent, child, index);
+		}
+	}
+
+	public void onConstructModified(Construct construct) {
+		// TODO Auto-generated method stub
+		if(this.isConstructInDocument(construct)) { 
+			mListener.onConstructModified(construct);
+		}
+	}
+}
