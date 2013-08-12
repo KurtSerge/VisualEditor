3e4549ae5f03028d6a7398f84098e1c944ec9548
Christopher Lord <chris.l.lord@gmail.com>
2013-08-12 13:06:29 +0100

    Automatic deletion

--- EditorDemo/src/clojure/constructs/StringLiteralConstruct.java
+++ EditorDemo/src/clojure/constructs/StringLiteralConstruct.java
@@ -1,9 +1,11 @@
 package clojure.constructs;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 import clojure.ClojureConstruct;
 import editor.Construct;
+import editor.Construct.ConstructAction;
 import editor.document.ConstructDocument;
 public class StringLiteralConstruct extends ClojureConstruct {
 	public StringLiteralConstruct(ConstructDocument document, ClojureConstruct parent, String literal) { 
@@ -25,4 +27,21 @@ public class StringLiteralConstruct extends ClojureConstruct {
 	public Color debug_getForegroundColor() {
 		return new Color(144, 33, 104);
 	}
+	
+	public ConstructAction onReceivedKeyEvent(KeyEvent keyEvent, boolean isTyping) {
+		if(isTyping) {
+			if((int)keyEvent.getKeyChar() == 34) { 
+				// Quotes are not allowed
+				System.out.println("TODO: Allow quotes, but automatically escape them");
+				return ConstructAction.ConsumeEvent;
+			}
+
+			if((int)keyEvent.getKeyChar() == 8 && this.literal.length() == 0) {
+				keyEvent.consume();
+				return ConstructAction.DeleteThis;
+			}
+		}
+
+		return ConstructAction.None;
+	}	
 }
