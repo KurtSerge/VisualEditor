c063f1fa33bfd1aa49ed593caecca72a9523a787
Christopher Lord <chris.l.lord@gmail.com>
2013-08-15 19:17:09 +0100

    Separate Pair into utility bundle

--- EditorDemo/src/editor/InterfaceController.java
+++ EditorDemo/src/editor/InterfaceController.java
@@ -10,6 +10,8 @@ import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import util.Pair;
+
 import autocomplete.AutoCompleteDialog.SimpleAutoCompleteEntry;
 import autocomplete.IAutoCompleteListener;
 
@@ -26,60 +28,6 @@ import editor.document.ConstructDocument;
  * @author Christopher Lord
  */
 public class InterfaceController implements KeyListener, IInterfaceActionListener, IAutoCompleteListener {
-
-	public static class Pair<A, B> {
-	    private A first;
-	    private B second;
-
-	    public Pair(A first, B second) {
-	    	super();
-	    	this.first = first;
-	    	this.second = second;
-	    }
-
-	    public int hashCode() {
-	    	int hashFirst = first != null ? first.hashCode() : 0;
-	    	int hashSecond = second != null ? second.hashCode() : 0;
-
-	    	return (hashFirst + hashSecond) * hashSecond + hashFirst;
-	    }
-
-	    public boolean equals(Object other) {
-	    	if (other instanceof Pair) {
-	    		Pair otherPair = (Pair) other;
-	    		return 
-	    		((  this.first == otherPair.first ||
-	    			( this.first != null && otherPair.first != null &&
-	    			  this.first.equals(otherPair.first))) &&
-	    		 (	this.second == otherPair.second ||
-	    			( this.second != null && otherPair.second != null &&
-	    			  this.second.equals(otherPair.second))) );
-	    	}
-
-	    	return false;
-	    }
-
-	    public String toString()
-	    { 
-	           return "(" + first + ", " + second + ")"; 
-	    }
-
-	    public A getFirst() {
-	    	return first;
-	    }
-
-	    public void setFirst(A first) {
-	    	this.first = first;
-	    }
-
-	    public B getSecond() {
-	    	return second;
-	    }
-
-	    public void setSecond(B second) {
-	    	this.second = second;
-	    }
-	}
 	
 	private LinkedList<IInterfaceActionListener> mActionListeners;
 	private LinkedList<Pair<Hotkey, EInterfaceAction>> mHotkeys;
@@ -282,7 +230,12 @@ public class InterfaceController implements KeyListener, IInterfaceActionListene
 			return ;
 		}
 		
-		Hotkey emulatedHotkey = new Hotkey(event.getKeyCode(), event.isControlDown(), event.isAltDown(), event.isShiftDown());
+		boolean isPrimaryTriggerDown = event.isControlDown(); 
+		if(isMac()) { 
+			isPrimaryTriggerDown = event.isMetaDown();
+		}
+		
+		Hotkey emulatedHotkey = new Hotkey(event.getKeyCode(), isPrimaryTriggerDown, event.isAltDown(), event.isShiftDown());
 		if(mHotkeySequenceCandidates == null) {
 			// There is no current sequence, so find all candidates
 			// that start with the current hotkey instance
@@ -562,4 +515,11 @@ public class InterfaceController implements KeyListener, IInterfaceActionListene
 
 	@Override
 	public void keyTyped(KeyEvent arg0) { }	
+	
+	
+	private static String OS = System.getProperty("os.name").toLowerCase();
+	
+	public static boolean isMac() {
+		return (OS.indexOf("mac") >= 0);
+	}
 }
