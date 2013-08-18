815c9b7abce261a15d942b08ba961d9eb5024ee4
Christopher Lord <chris.l.lord@gmail.com>
2013-08-18 17:08:31 +0100

    Map restriction

--- EditorDemo/src/clojure/constructs/meta/DefineFunctionConstruct.java
+++ EditorDemo/src/clojure/constructs/meta/DefineFunctionConstruct.java
@@ -1,9 +1,12 @@
 package clojure.constructs.meta;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 
 import clojure.constructs.StringConstruct;
 import clojure.constructs.containers.ListConstruct;
+import clojure.constructs.containers.MapConstruct;
 import construct.Construct;
 import construct.Placeholder;
 import construct.Construct.SelectionCause;
@@ -28,7 +31,7 @@ public class DefineFunctionConstruct extends MetaConstruct {
 		LinkedList<Placeholder> placeholders = new LinkedList<Placeholder>();
 		placeholders.add(Placeholder.createPlaceholder("name"));
 		placeholders.add(Placeholder.createOptionalPlaceholder("doc-string", StringConstruct.class));
-		placeholders.add(Placeholder.createOptionalPlaceholder("attr-map"));
+		placeholders.add(Placeholder.createOptionalPlaceholder("attr-map", MapConstruct.class));
 		placeholders.add(Placeholder.createPermanentPlaceholder(new VariadicVectorConstruct(mDocument, this, "params")));
 		placeholders.add(Placeholder.createVariadicPlaceholder("exprs"));
 		setPlaceholders(placeholders);
@@ -49,5 +52,5 @@ public class DefineFunctionConstruct extends MetaConstruct {
 		}
 		
 		return super.getConstructForSelection(type);
-	}		
+	}
 }
